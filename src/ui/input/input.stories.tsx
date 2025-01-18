c9092b10b20c5a79618223e947c247a17be8d198
DongjaJ <jdh981118@naver.com>
2025-01-18 18:54:26 +0900

    feat: input 컴포넌트 스토리 추가

---
+++ src/ui/input/input.stories.tsx
@@ -0,0 +1,72 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { DeleteCir } from '../../assets/index.ts';
+import { InputContainer } from './input-container.tsx';
+import { InputRightElement } from './input-element.tsx';
+import { Input } from './input.tsx';
+
+const meta: Meta<typeof Input> = {
+  title: 'ui/Input',
+  component: Input,
+  parameters: {
+    layout: 'centered',
+  },
+  argTypes: {
+    value: {
+      control: 'text',
+    },
+    placeholder: {
+      control: 'text',
+    },
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof Input>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const OnlyInput: Story = {
+  render: (args) => (
+    <InputContainer>
+      <Input {...args} />
+    </InputContainer>
+  ),
+};
+
+export const InputWithButton: Story = {
+  render: (args) => (
+    <InputContainer
+      style={{
+        width: '335px',
+      }}
+    >
+      <Input {...args} />
+      <InputRightElement>
+        <DeleteCir />
+      </InputRightElement>
+    </InputContainer>
+  ),
+};
+
+export const ErrorInput: Story = {
+  args: {
+    variant: 'error',
+    value: '에러 텍스트입니다',
+  },
+  render: (args) => (
+    <InputContainer>
+      <Input {...args} />
+    </InputContainer>
+  ),
+};
+
+export const DisabledInput: Story = {
+  args: {
+    value: 'disabled',
+    disabled: true,
+  },
+  render: (args) => (
+    <InputContainer>
+      <Input {...args} />
+    </InputContainer>
+  ),
+};
