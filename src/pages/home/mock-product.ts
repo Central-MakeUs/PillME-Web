2b137fbc94d86a5346cf52d535de94166ba1a1d9
DongjaJ <jdh981118@naver.com>
2025-01-28 02:05:29 +0900

    feat: 목 데이터 추가

---
+++ src/pages/home/mock-product.ts
@@ -0,0 +1,8 @@
+export const MOCK_PRODUCT = {
+  imageUrl: 'https://picsum.photos/200/300',
+  company: '종근당건강 종근당건강 종근당건강',
+  name: '이뮨베라 이뮨베라 이뮨베라 이뮨베라 이뮨베라 이뮨베라 이뮨베라 ',
+  price: 77000,
+};
+
+export const MOCK_PRODUCT_LIST = Array.from({ length: 10 }, () => MOCK_PRODUCT);
