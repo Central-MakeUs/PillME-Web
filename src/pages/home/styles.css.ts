16c8163bb8dfd266825d6b0d64e4f29ce8d10897
DongjaJ <jdh981118@naver.com>
2025-01-28 02:08:03 +0900

    feat: 홈페이지 기초 레이아웃 및 스타일링 설정

--- src/pages/home/styles.css.ts
+++ src/pages/home/styles.css.ts
@@ -1,4 +1,129 @@
 import { style } from '@vanilla-extract/css';
-import { color } from '../../ui';
+import { color, typography } from '../../ui';
 
 export const home = style({ backgroundColor: color('blue100') });
+
+export const mainContainer = style({
+  padding: '60px 20px 86px 20px',
+});
+
+export const contentContainer = style({
+  display: 'flex',
+  gap: 18,
+  flexDirection: 'column',
+});
+
+export const mainSearchContainer = style([
+  contentContainer,
+  {
+    alignItems: 'center',
+  },
+]);
+
+export const recommendSearchTitle = style([
+  typography('head_1_18_sb'),
+  {
+    color: color('grey900'),
+  },
+]);
+
+export const recommendSearchDescription = style([
+  typography('body_5_12_r'),
+  {
+    color: color('grey500'),
+  },
+]);
+
+export const recommendSearchKeywordContainer = style([
+  contentContainer,
+  {
+    gap: 15,
+  },
+]);
+
+export const bottomSheetContainer = style({
+  backgroundColor: color('white'),
+  borderRadius: '20px 20px 0px 0px',
+  boxShadow: '0 -1.01px 12.3px 5px rgba(148, 161, 201, 0.3)',
+});
+
+export const bottomSheetHealthCategoryContainer = style({
+  padding: '16px 20px',
+});
+
+export const viewAllButton = style({
+  width: '100%',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  padding: '8px 0',
+  borderTop: `1px solid ${color('grey100')}`,
+  cursor: 'pointer',
+});
+
+export const separator = style({
+  height: 10,
+  backgroundColor: color('grey100'),
+});
+
+export const bottomSheetDescription = style([
+  typography('body_3_14_r'),
+  {
+    color: color('grey600'),
+  },
+]);
+
+export const recommendCategoryContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 14,
+});
+
+export const recommendCategoryGallery = style({
+  display: 'grid',
+  gridTemplateColumns: 'repeat(4, 1fr)',
+  gap: '18px 32px',
+});
+
+export const recommendProductContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 50,
+  padding: '30px 20px 36px 20px',
+});
+
+export const recommendProductGalleryContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 18,
+  overflow: 'hidden',
+});
+
+export const recommendProductGalleryHeader = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 2,
+  padding: '5px 0 8px 0',
+});
+
+export const recommendProductGalleryHeaderTitleContainer = style({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignContent: 'center',
+});
+
+export const recommendProductGallery = style({
+  display: 'flex',
+  gap: 18,
+  overflowX: 'auto',
+  width: 'calc(100dvw - 40px)',
+  '@media': {
+    'screen and (min-width: 440px)': {
+      width: 400,
+    },
+  },
+  whiteSpace: 'nowrap',
+  '::-webkit-scrollbar': {
+    display: 'none',
+  },
+});
