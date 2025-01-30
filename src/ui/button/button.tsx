68075d2cfbadef7a76a2dedb0aa8465cfaad5e11
DongjaJ <jdh981118@naver.com>
2025-01-30 23:57:49 +0900

    fix: button props 및 스타일 수정

--- src/ui/button/button.tsx
+++ src/ui/button/button.tsx
@@ -1,26 +1,35 @@
-import { ComponentProps, PropsWithChildren } from 'react';
+import { ComponentProps, PropsWithChildren, ReactNode } from 'react';
 import { ArrowRightr, Plus } from '../../assets';
 import { cx } from '../util';
 import * as styles from './button.styles.css';
 
-export type ButtonProps = ComponentProps<'button'> & styles.ButtonVariants;
+export type ButtonProps = ComponentProps<'button'> &
+  styles.ButtonVariants & {
+    left?: ReactNode;
+    right?: ReactNode;
+  };
 
 export const Button = ({
   className,
   variant,
   size,
   disabled,
+  left,
+  right,
   children,
   ...restProps
 }: PropsWithChildren<ButtonProps>) => {
-  const leftIcon =
+  const defaultLeftIcon =
     variant === 'third' && (size === 'small' || size === 'large') ? (
       <Plus width={20} height={20} />
     ) : null;
-  const rightIcon =
+  const defaultRightIcon =
     variant === 'primary' && size === 'small' ? (
       <ArrowRightr width={24} height={24} />
     ) : null;
+
+  const leftIcon = left ?? defaultLeftIcon;
+  const rightIcon = right ?? defaultRightIcon;
 
   return (
     <button
