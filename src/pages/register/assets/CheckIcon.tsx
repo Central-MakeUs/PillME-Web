7c37be17037e3707d4d3ad57f379d3e937775d07
DongjaJ <jdh981118@naver.com>
2025-01-30 16:50:44 +0900

    feat: 약관동의 페이지 추가

---
+++ src/pages/register/assets/CheckIcon.tsx
@@ -0,0 +1,19 @@
+export const CheckIcon = () => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 25 25"
+      fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M4.0376 11.3917L9.37093 16.7917L20.0376 7.79175"
+        stroke="white"
+        strokeWidth="2.57143"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
