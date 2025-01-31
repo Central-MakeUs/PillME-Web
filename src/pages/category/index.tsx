4d44c02c0b5796196bc5f96f868251322e4366bb
DongjaJ <jdh981118@naver.com>
2025-02-01 01:07:57 +0900

    feat: 카테고리 검색 관련 페이지 라우팅 추가

---
+++ src/pages/category/index.tsx
@@ -0,0 +1,5 @@
+import { PageLayout } from '@/ui/layout/page-layout';
+
+export const CategoryPage = () => {
+  return <PageLayout>카테고리 선택 페이지</PageLayout>;
+};
---
+++ src/pages/category/result/index.tsx
@@ -0,0 +1,5 @@
+import { PageLayout } from '@/ui/layout/page-layout';
+
+export const CategoryResultPage = () => {
+  return <PageLayout>카테고리 검색 결과 페이지</PageLayout>;
+};
