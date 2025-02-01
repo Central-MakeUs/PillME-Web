fd4a52b81fc95b28fd10b46c5fd1b7f7f5e521fa
DongjaJ <jdh981118@naver.com>
2025-02-01 21:44:54 +0900

    feat: icon button 컴포넌트 추가

---
+++ src/ui/icon-button/icon-button.css.ts
@@ -0,0 +1,9 @@
+import { style } from '@vanilla-extract/css';
+
+export const iconButton = style({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: 44,
+  height: 44,
+});
