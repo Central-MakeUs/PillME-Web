545bc19d4cc9905913f7c577a501cc4012eba913
박태규 Park Tae Kyu <ptq124@naver.com>
2025-02-04 00:53:55 +0900

    feat: 제품상세페이지 추가

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,3 +1,101 @@
+import { useNavigate, useParams } from 'react-router';
+import { ArrowLeft, Cart } from '@/assets';
+import { AppBar } from '@/ui/app-bar';
+import { Button } from '@/ui/button';
+import { ButtonText } from '@/ui/button-text';
+import { Chip } from '@/ui/chip';
+import { PageLayout } from '@/ui/layout/page-layout';
+import { Spacer } from '@/ui/spacer/spacer';
+import { MOCK_PRODUCT_LIST } from '../home/mock-product';
+import { IngredientCard } from './components/ingredient-card';
+import { IngredientChart } from './components/ingredient-chart';
+import * as styles from './style.css';
+
 export const ProductPage = () => {
-  return <>ProductPage</>;
+  const navigate = useNavigate();
+  const { productId } = useParams();
+  const product = MOCK_PRODUCT_LIST.find((p) => p.id === Number(productId));
+
+  if (!product) return <p>상품을 찾을 수 없습니다.</p>; // not-found 페이지로 교체
+
+  return (
+    <PageLayout
+      header={
+        <div>
+          <AppBar
+            left={<ArrowLeft onClick={() => navigate(-1)} />}
+            right={<Cart />}
+            variant="page"
+          />
+          <div className={styles.separator} />
+        </div>
+      }
+    >
+      <section className={styles.productContainer}>
+        <img src={product.imageUrl} className={styles.productImg} alt="제품" />
+        <div className={styles.info}>
+          <div className={styles.subTitle}>웰릿</div>
+          <div className={styles.title}>
+            윌릿나토킨NATOKIN윌릿나토킨NATOKIN윌릿나토킨NATOKIN
+          </div>
+          <div className={styles.price}>
+            77,000 <span className={styles.won}>원</span>
+          </div>
+          <div className={styles.tags}>
+            <Chip shape="pill" color="blue400" backgroundColor="blue200">
+              혈압
+            </Chip>
+            <Chip shape="pill" color="grey500" backgroundColor="grey200">
+              빈혈
+            </Chip>
+            <Chip shape="pill" color="grey500" backgroundColor="grey200">
+              루테인
+            </Chip>
+          </div>
+        </div>
+        <div className={styles.pillButtonBox}>
+          <Button size="small" variant="third" className={styles.pillButton}>
+            내 약통
+          </Button>
+        </div>
+      </section>
+      <Spacer size={10} className={styles.spaceColor} />
+      <section className={styles.chartContainer}>
+        <div className={styles.chartHeader}>
+          <div className={styles.sectionTitle}>제품 성분 분포도</div>
+          <ButtonText
+            icon
+            onClick={() => navigate(`/product/${product.id}/ingredient`)}
+          >
+            성분 전체보기
+          </ButtonText>
+        </div>
+        <div className={styles.chartBox}>
+          <IngredientChart />
+        </div>
+      </section>
+      <Spacer size={10} className={styles.spaceColor} />
+      <section className={styles.compareContainer}>
+        <div className={styles.sectionTitle}>
+          제품의 총 19개의 성분 중<br />
+          필미님은 4개를 충족해요
+        </div>
+        <div className={styles.ingredientCards}>
+          {/* number props 추가 */}
+          <IngredientCard status="충족" />
+          <IngredientCard status="부족" />
+          <IngredientCard status="과다" />
+        </div>
+      </section>
+      <div className={styles.buttonWrapper}>
+        <Button size="large" variant="secondary">
+          구매하러 가기
+        </Button>
+        {/* 토스트 알럿 추가 */}
+        <Button size="large" variant="primary">
+          장바구니 담기
+        </Button>
+      </div>
+    </PageLayout>
+  );
 };
---
+++ src/pages/product/style.css.ts
@@ -0,0 +1,116 @@
+import { style } from '@vanilla-extract/css';
+import { color, typography } from '@/ui';
+
+export const separator = style({
+  marginTop: 10,
+});
+
+export const productContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 20,
+  width: '100%',
+  marginBottom: 20,
+});
+export const productImg = style({
+  width: '100%',
+  height: 350,
+});
+export const info = style({
+  padding: '0 20px',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 6,
+});
+export const subTitle = style([
+  {
+    color: color('grey600'),
+  },
+  typography('head_3_16_r'),
+]);
+export const title = style([
+  {
+    color: color('grey800'),
+    whiteSpace: 'nowrap',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+  },
+  typography('head_1_18_sb'),
+]);
+
+export const price = style([
+  {
+    color: color('grey800'),
+  },
+  typography('title_2_20_b'),
+]);
+export const won = style([
+  {
+    color: color('grey800'),
+  },
+  typography('head_1_18_sb'),
+]);
+export const tags = style([
+  {
+    display: 'flex',
+    gap: 4,
+  },
+]);
+export const pillButtonBox = style({
+  padding: '0 20px',
+});
+
+export const pillButton = style({
+  width: '100%',
+});
+
+export const spaceColor = style({
+  background: color('grey100'),
+});
+
+export const chartContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 40,
+  width: '100%',
+  padding: '30px 20px 20px 20px',
+});
+
+export const chartHeader = style({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+});
+
+export const sectionTitle = style([
+  {
+    color: color('grey800'),
+  },
+  typography('title_2_20_b'),
+]);
+
+export const chartBox = style({
+  width: '100%',
+  display: 'flex',
+  justifyContent: 'center',
+});
+export const compareContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 25,
+  padding: '30px 35px 20px 20px',
+  borderBottom: '1px solid',
+  borderBottomColor: color('grey100'),
+});
+
+export const ingredientCards = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 20,
+});
+
+export const buttonWrapper = style({
+  display: 'flex',
+  gap: 15,
+  padding: '14px 34px 20px ',
+});
