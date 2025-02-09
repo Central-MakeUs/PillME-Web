f61f76202d1deff1bab54db98c18447164f1a1ee
DongjaJ <jdh981118@naver.com>
2025-02-09 23:26:41 +0900

    feat: 마이 페이지 퍼블리싱 추가

--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -1,3 +1,80 @@
+import { Fragment } from 'react/jsx-runtime';
+import { useNavigate } from 'react-router';
+import { ArrowRightr, Cart } from '@/assets';
+import { AppBar, AppBarElement } from '@/ui/app-bar';
+import { PageLayout } from '@/ui/layout/page-layout';
+import { Spacer } from '@/ui/spacer/spacer';
+import { EmailIcon } from '../onboarding/assets/EmailIcon';
+import * as styles from './page.css';
+
 export const MyPage = () => {
-  return <>MyPage</>;
+  const navigate = useNavigate();
+
+  const goCartPage = () => navigate('/cart');
+
+  return (
+    <PageLayout
+      header={
+        <AppBar
+          right={
+            <AppBarElement onClick={goCartPage}>
+              <Cart />
+            </AppBarElement>
+          }
+          className={styles.appBar}
+        >
+          마이페이지
+        </AppBar>
+      }
+    >
+      <header className={styles.header}>
+        <div>
+          <h2 className={styles.name}>김필미</h2>
+          <Spacer size={10} />
+          <div className={styles.emailContainer}>
+            <EmailIcon />
+            <p className={styles.email}>pillme1234@naver.com</p>
+          </div>
+        </div>
+        <button>
+          <ArrowRightr />
+        </button>
+      </header>
+      {CONTENT_LIST.map(({ title, listItemList }) => (
+        <Fragment key={title}>
+          <div className={styles.separator} />
+          <section className={styles.listContainer}>
+            <h3 className={styles.listTitle}>{title}</h3>
+            <Spacer size={20} />
+            <div className={styles.listItemContainer}>
+              {listItemList.map(({ text }) => (
+                <div className={styles.item} key={text}>
+                  <p className={styles.itemText}>{text}</p>
+                  <button>
+                    <ArrowRightr />
+                  </button>
+                </div>
+              ))}
+            </div>
+          </section>
+        </Fragment>
+      ))}
+    </PageLayout>
+  );
 };
+
+//TODO 라우팅할 path 추가
+const CONTENT_LIST = [
+  {
+    title: '서비스',
+    listItemList: [{ text: '나의 상담 기록' }, { text: '내 약통 관리' }],
+  },
+  {
+    title: '고객센터',
+    listItemList: [
+      { text: '공지사항' },
+      { text: '이용약관' },
+      { text: '1:1 카톡 채널 문의' },
+    ],
+  },
+];
