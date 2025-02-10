caafb25c2a9b21351e87494763b7af176814b84e
박태규 Park Tae Kyu <ptq124@naver.com>
2025-02-11 00:16:04 +0900

    fix: 선택 건너뛰기 navigate 추가

--- src/pages/consultation/components/apply.tsx
+++ src/pages/consultation/components/apply.tsx
@@ -1,13 +1,21 @@
+import { useNavigate } from 'react-router';
 import * as styles from './apply.css';
 import { PharmacistCard } from './pharmacist-card';
 
 // 상담신청 탭
 export const Apply = () => {
+  const navigate = useNavigate();
+
   return (
     <section className={styles.container}>
       <div className={styles.header}>
         <div className={styles.title}>약사님의 선택</div>
-        <button className={styles.btn}>선택 건너뛰기</button>
+        <button
+          className={styles.btn}
+          onClick={() => navigate('/consultation/new')}
+        >
+          선택 건너뛰기
+        </button>
       </div>
       <div className={styles.cardContianer}>
         <PharmacistCard />
