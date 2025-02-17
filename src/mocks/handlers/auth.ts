25c35a58f2443f956ebbdb0bd601e28963e0dfc8
DongjaJ <jdh981118@naver.com>
2025-02-17 19:22:38 +0900

    feat: 이메일 중복체크, 코드 전송, 코드 인증 mock api 추가

--- src/mocks/handlers/auth.ts
+++ src/mocks/handlers/auth.ts
@@ -13,4 +13,16 @@ export const handlers = [
       { status: 200 },
     );
   }),
+  http.post('/auth/email/check-duplicated', async () => {
+    await delay(200);
+    return HttpResponse.json(null, { status: 200 });
+  }),
+  http.post('/auth/email/send-code', async () => {
+    await delay(200);
+    return HttpResponse.json(null, { status: 200 });
+  }),
+  http.post('/auth/email/vertify-code', async () => {
+    await delay(200);
+    return HttpResponse.json(null, { status: 200 });
+  }),
 ];
