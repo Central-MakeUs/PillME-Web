ad12544fb608674228ede563d9eb0e92d42fae37
DongjaJ <jdh981118@naver.com>
2025-02-18 22:58:16 +0900

    feat: 상품 검색 mock 핸들러 추가

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -1,4 +1,5 @@
 import { setupWorker } from 'msw/browser';
 import { handlers as authHandlers } from './handlers/auth';
+import { handlers as productHandlers } from './handlers/product';
 
-export const worker = setupWorker(...authHandlers);
+export const worker = setupWorker(...authHandlers, ...productHandlers);
