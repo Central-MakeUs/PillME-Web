cd92d4b2eec4daff273ba41e11da6d49665405fe
DongjaJ <jdh981118@naver.com>
2025-02-19 21:57:24 +0900

    feat: user 관련 mock api 추가

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -1,5 +1,10 @@
 import { setupWorker } from 'msw/browser';
 import { handlers as authHandlers } from './handlers/auth';
 import { handlers as productHandlers } from './handlers/product';
+import { handlers as userHandlers } from './handlers/user';
 
-export const worker = setupWorker(...authHandlers, ...productHandlers);
+export const worker = setupWorker(
+  ...authHandlers,
+  ...productHandlers,
+  ...userHandlers,
+);
---
+++ src/mocks/handlers/user.ts
@@ -0,0 +1,34 @@
+import { HttpResponse, delay, http } from 'msw';
+
+export const handlers = [
+  http.get('user/info', async () => {
+    await delay(200);
+    return HttpResponse.json(
+      {
+        data: {
+          id: Date.now(),
+          email: 'pillme@acer.com',
+          nickname: 'pillme',
+          birthDate: '2025.02.19',
+          gender: 'FEMALE',
+        },
+      },
+      { status: 200 },
+    );
+  }),
+  http.post('user/info', async () => {
+    await delay(200);
+    return HttpResponse.json(
+      {
+        data: {
+          id: Date.now(),
+          email: 'pillme@acer.com',
+          nickname: 'pillme',
+          birthDate: '2025.02.19',
+          gender: 'FEMALE',
+        },
+      },
+      { status: 200 },
+    );
+  }),
+];
