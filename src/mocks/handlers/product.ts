ece0cd17dda25dc09d9bf2ab976be44f6e6451c1
DongjaJ <jdh981118@naver.com>
2025-02-24 22:51:51 +0900

    refactor: 핸들러에서 목 데이터 분리

--- src/mocks/handlers/product.ts
+++ src/mocks/handlers/product.ts
@@ -1,43 +1,12 @@
 import { HttpResponse, delay, http } from 'msw';
+import { MOCK_PRODUCT } from '@/fixture/product';
 
 export const handlers = [
   http.get('/product', async () => {
     await delay(200);
     return HttpResponse.json(
       {
-        data: [
-          {
-            id: Date.now(),
-            name: '이뮨베라 이뮨베라 이뮨베라 이뮨베라 이뮨베라 이뮨베라 이뮨베라 ',
-            price: 70_000,
-            imageUrl: 'https://picsum.photos/200/300',
-            purchaseLink: 'string',
-            description: '종근당건강 종근당건강 종근당건강', //TODO 회사 이름인지 문의 필요
-            isActive: true,
-            healthConcerns: [
-              {
-                id: 0,
-                name: 'string',
-                description: 'string',
-                imageUrl: 'string',
-                ingredients: [
-                  {
-                    id: 0,
-                    name: 'string',
-                  },
-                ],
-              },
-            ],
-            productIngredients: [
-              {
-                ingredientId: 0,
-                ingredientName: 'string',
-                amount: 0,
-                unit: 'string',
-              },
-            ],
-          },
-        ],
+        data: Array.from({ length: 20 }, () => MOCK_PRODUCT),
       },
       { status: 200 },
     );
