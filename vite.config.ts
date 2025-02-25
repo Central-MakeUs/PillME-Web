60b7e9dc8e19b8da449a9226b6ef6b9b0e9b1914
DongjaJ <jdh981118@naver.com>
2025-02-25 17:37:47 +0900

    chore: pwa에 필요한 에셋 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,54 @@
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import svgr from 'vite-plugin-svgr';
 import tsConfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), tsConfigPaths(), vanillaExtractPlugin()],
+  plugins: [
+    react(),
+    svgr(),
+    tsConfigPaths(),
+    vanillaExtractPlugin(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      injectRegister: 'script-defer',
+      manifest: {
+        name: 'PillME: 건강기능식품 추천 & 관리',
+        short_name: 'PillME',
+        description:
+          '개인 건강 고민/상태에 맞춘 영양제 추천과 소분 서비스를 제공하는 스마트 헬스 플랫폼',
+        theme_color: '#fff',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
 });
