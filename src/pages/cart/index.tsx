23046df2b9155395eeb1a0d05c86a15d6fba1a87
DongjaJ <jdh981118@naver.com>
2025-03-02 14:45:46 +0900

    feat: 장바구니 조회 api 연동

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,8 +1,10 @@
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, Suspense, useState } from 'react';
+import { useSuspenseQuery } from '@tanstack/react-query';
 import { motion } from 'motion/react';
 import { useNavigate } from 'react-router';
+import { cartQueryOption } from '@/apis/query/cart';
 import { ArrowLeft } from '@/assets';
-import { MOCK_PRODUCT_LIST } from '@/pages/home/mock-product';
+import { LocalErrorBoundary } from '@/components/LocalErrorBoundary';
 import { AppBar } from '@/ui/app-bar';
 import { Button } from '@/ui/button';
 import { ButtonText } from '@/ui/button-text';
@@ -12,23 +14,39 @@ import { Dialog } from '@/ui/dialog';
 import { IconButton } from '@/ui/icon-button';
 import { PageLayout } from '@/ui/layout/page-layout';
 import { Spacer } from '@/ui/spacer/spacer';
+import { name } from '../userinfo/schema';
 import * as styles from './page.css';
 
 export const CartPage = () => {
+  return (
+    <LocalErrorBoundary>
+      <Suspense>
+        <CartPageInner />
+      </Suspense>
+    </LocalErrorBoundary>
+  );
+};
+
+const CartPageInner = () => {
   const navigate = useNavigate();
 
   const goBack = () => navigate(-1);
 
-  const [productList, setProductList] = useState(
-    MOCK_PRODUCT_LIST.map((product) => ({
-      ...product,
-      checked: true,
-    })),
-  );
+  const {
+    data: { data: cartList },
+  } = useSuspenseQuery(cartQueryOption.list());
+
+  const initialProductList = cartList.map(({ product, cartId }) => ({
+    ...product,
+    checked: true,
+    cartId,
+  }));
+
+  const [productList, setProductList] = useState(initialProductList);
 
   const toggleCheck = ({ target: { id } }: ChangeEvent) => {
     const updatedProductList = productList.map((product) =>
-      product.id === Number(id)
+      product.cartId === Number(id)
         ? { ...product, checked: !product.checked }
         : product,
     );
@@ -100,28 +118,32 @@ export const CartPage = () => {
         </div>
         <Spacer size={18} />
         <div className={styles.list}>
-          {/* TODO 가격 UI 추가 필요  */}
-          {productList.map(({ id, checked, ...rest }) => (
-            <HorizontalCard
-              {...rest}
-              key={id}
-              label={
-                <Checkbox
-                  id={String(id)}
-                  checked={checked}
-                  onChange={toggleCheck}
-                  className={styles.checkbox}
-                />
-              }
-            >
-              <p className={styles.price}>
-                <span className={styles.priceNumber}>
-                  {Number(10000).toLocaleString()}
-                </span>
-                원
-              </p>
-            </HorizontalCard>
-          ))}
+          {/* TODO x 버튼 핸들러는 추후 추가 예정 */}
+          {productList.map(
+            ({ checked, cartId, description, imageUrl, price, name }) => (
+              <HorizontalCard
+                key={cartId}
+                label={
+                  <Checkbox
+                    id={String(cartId)}
+                    checked={checked}
+                    onChange={toggleCheck}
+                    className={styles.checkbox}
+                  />
+                }
+                company={description}
+                imageUrl={imageUrl}
+                name={name}
+              >
+                <p className={styles.price}>
+                  <span className={styles.priceNumber}>
+                    {Number(price).toLocaleString()}
+                  </span>
+                  원
+                </p>
+              </HorizontalCard>
+            ),
+          )}
         </div>
       </motion.div>
       {checkedCount !== 0 && (
