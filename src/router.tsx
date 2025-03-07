e9aa4177d0f38b849d3d17c85cc9637811f3c926
DongjaJ <jdh981118@naver.com>
2025-03-07 22:08:43 +0900

    feat: 로그인 안하고 권한 필요한 페이지 접근 시 로그인 페이지로 리다이렉트

--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
 import { NotFoundPage } from '@/pages/not-found';
+import PrivateRoute from './components/PrivateRoute';
 import { CartPage } from './pages/cart';
 import { CategoryPage } from './pages/category';
 import { CategoryResultPage } from './pages/category/result';
@@ -17,8 +18,6 @@ import { PillboxManagePage } from './pages/pillbox/manage';
 import { PillboxNewPage } from './pages/pillbox/new';
 import { ProductPage } from './pages/product';
 import { ProductIngredientPage } from './pages/product/ingredient';
-import { RankingPage } from './pages/ranking';
-import { RecommendPage } from './pages/recommend';
 import { RegisterPage } from './pages/register';
 import { SearchPage } from './pages/search';
 import { SearchResultPage } from './pages/search/result';
@@ -34,37 +33,25 @@ export const Router = () => {
           element={<OnboardingPage />}
           errorElement={<NotFoundPage />}
         />
+        {/* Public Route */}
         <Route element={<BottomNavigationLayout />}>
           <Route path="home" element={<HomePage />} />
-          <Route path="pillbox" element={<PillboxPage />} />
-          <Route path="consultation" element={<ConsultationPage />} />
-          <Route path="mypage" element={<MyPage />} />
         </Route>
+
         <Route path="login" element={<LoginPage />} />
         <Route path="register" element={<RegisterPage />} />
-        <Route path="userinfo" element={<UserInfoPage />} />
+
         <Route path="search">
           <Route path=":searchType">
             <Route index element={<SearchPage />} />
             <Route path="result" element={<SearchResultPage />} />
           </Route>
         </Route>
+
         <Route path="product">
           <Route path=":productId">
             <Route index element={<ProductPage />} />
             <Route path="ingredient" element={<ProductIngredientPage />} />
-          </Route>
-        </Route>
-        <Route path="pillbox">
-          <Route path="manage" element={<PillboxManagePage />} />
-          <Route path="new" element={<PillboxNewPage />} />
-        </Route>
-
-        <Route path="consultation">
-          <Route path=":consultId" element={<ConsultationDetailPage />} />
-          <Route path="new">
-            <Route index element={<ConsultationNewPage />} />
-            <Route path="complete" element={<ConsultationCompletePage />} />
           </Route>
         </Route>
 
@@ -73,10 +60,32 @@ export const Router = () => {
           <Route path=":categoryId" element={<CategoryResultPage />} />
         </Route>
 
-        <Route path="ranking" element={<RankingPage />} />
-        <Route path="recommend" element={<RecommendPage />} />
-        <Route path="cart" element={<CartPage />} />
-        <Route path="mypage/manage" element={<MyInfoManagePage />} />
+        {/* Private Route */}
+        <Route element={<PrivateRoute />}>
+          <Route element={<BottomNavigationLayout />}>
+            <Route path="pillbox" element={<PillboxPage />} />
+            <Route path="consultation" element={<ConsultationPage />} />
+            <Route path="mypage" element={<MyPage />} />
+          </Route>
+
+          <Route path="userinfo" element={<UserInfoPage />} />
+          <Route path="pillbox">
+            <Route path="manage" element={<PillboxManagePage />} />
+            <Route path="new" element={<PillboxNewPage />} />
+          </Route>
+
+          <Route path="consultation">
+            <Route path=":consultId" element={<ConsultationDetailPage />} />
+            <Route path="new">
+              <Route index element={<ConsultationNewPage />} />
+              <Route path="complete" element={<ConsultationCompletePage />} />
+            </Route>
+          </Route>
+
+          <Route path="cart" element={<CartPage />} />
+          <Route path="mypage/manage" element={<MyInfoManagePage />} />
+        </Route>
+
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
