967ca509dde699e37054bfa6142ccc497a8a7f96
DongjaJ <jdh981118@naver.com>
2025-03-07 23:52:52 +0900

    chore: 빌드 크기 문제로 lottie 코드 스플리팅으로 분리

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,16 @@ import tsConfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router'],
+          lottie: ['react-lottie-player'],
+        },
+      },
+    },
+  },
   plugins: [
     react(),
     svgr(),
